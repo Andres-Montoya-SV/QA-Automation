c69987a130385a8d1812d4d15be514dbd90feb40
Andres-Montoya-SV <andreww470000@gmail.com>
2023-04-06 01:52:08 -0600

    Separated tests

--- test/specs/focusServices.js
+++ test/specs/focusServices.js
@@ -3,7 +3,7 @@ const GooglePage = require("../pages/GooglePage");
 const FocusServicesPage = require("../pages/FocusServicesPage");
 
 describe("Focus Services test", () => {
-  it("should search for Focus Services and validate elements", async () => {
+  before(async () => {
     await GooglePage.open();
     await GooglePage.search("Focus Services");
 
@@ -13,33 +13,41 @@ describe("Focus Services test", () => {
       "Focus Services link not found in search results"
     );
     await focusServicesLink.click();
+  });
 
+  it("should validate Now Hiring button", async () => {
     await FocusServicesPage.scrollToBottom();
 
     assert(
       await FocusServicesPage.nowHiringBtn.isDisplayed(),
       "Now Hiring button not found"
     );
+  });
 
+  it("should validate North America link", async () => {
     await FocusServicesPage.locationsTab.click();
 
     assert(
       await FocusServicesPage.northAmericaLink.isDisplayed(),
       "North America link not found"
     );
+  });
 
+  it("should validate Central America title", async () => {
     await FocusServicesPage.centralAmericaLink.click();
 
     assert(
       await FocusServicesPage.centralAmericaTitle.isDisplayed(),
       "Central America title not found"
     );
+  });
 
+  it("should validate Asia location text", async () => {
     await FocusServicesPage.asiaLink.click();
 
     assert(
       await FocusServicesPage.asiaLocationText.isDisplayed(),
-      "Asia location text not found"
+      "Bacolod City, Philippines - not found in Asia location text block"
     );
   });
 });
