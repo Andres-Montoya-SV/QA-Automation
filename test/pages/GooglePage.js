dd01a6ec9b7a79b1cfad2def2f97384368e6c688
Andres-Montoya-SV <andreww470000@gmail.com>
2023-04-06 00:37:16 -0600

    Added Google Page class

---
+++ test/pages/GooglePage.js
@@ -0,0 +1,19 @@
+class GooglePage {
+  async open() {
+    await browser.url("/");
+  }
+
+  async search(query) {
+    await (await this.searchInput).setValue(query);
+    await (await this.searchBtn).click();
+  }
+
+  get searchInput() {
+    return $('input[name="q"]');
+  }
+  get searchBtn() {
+    return $('input[type="submit"]');
+  }
+}
+
+module.exports = new GooglePage();
