8ba4a296c89f53af8333efd15d94d2fd038c2646
Andres-Montoya-SV <andreww470000@gmail.com>
2023-04-06 00:57:11 -0600

    FocusServices added to specs - first test run

---
+++ test/specs/focusServices.js
@@ -0,0 +1,44 @@
+const assert = require("assert");
+const GooglePage = require("../pages/GooglePage");
+const FocusServicesPage = require("../pages/FocusServicesPage");
+
+describe("Focus Services test", () => {
+  it("should search for Focus Services and validate elements", async () => {
+    await GooglePage.open();
+    await GooglePage.search("Focus Services");
+
+    assert(
+      await FocusServicesPage.focusServicesLink.isDisplayed(),
+      "Focus Services link not found in search results"
+    );
+    await FocusServicesPage.focusServicesLink.click();
+
+    await FocusServicesPage.scrollToBottom();
+
+    assert(
+      await FocusServicesPage.nowHiringBtn.isDisplayed(),
+      "Now Hiring button not found"
+    );
+
+    await FocusServicesPage.locationsTab.click();
+
+    assert(
+      await FocusServicesPage.northAmericaLink.isDisplayed(),
+      "North America link not found"
+    );
+
+    await FocusServicesPage.centralAmericaLink.click();
+
+    assert(
+      await FocusServicesPage.centralAmericaTitle.isDisplayed(),
+      "Central America title not found"
+    );
+
+    await FocusServicesPage.asiaLink.click();
+
+    assert(
+      await FocusServicesPage.asiaLocationText.isDisplayed(),
+      "Asia location text not found"
+    );
+  });
+});
